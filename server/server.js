809db60b1df2fbe1765ba33a87e7ec0e4d49c23f
Pranjal Rai <raipranjal2909@gmail.com>
2025-08-02 00:17:06 +0530

    Clerk Webhook

--- server/server.js
+++ server/server.js
@@ -2,14 +2,24 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebHooks from "./controller/clerkWebhooks.js";
 
 connectDB()
 
 const app = express()
 app.use(cors())
 
+// #middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+// API to listen clerk webhooks
+app.use("api/clerk", clerkWebHooks)
+
 app.get('/', (req, res)=> res.send("API is working "))
 
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
